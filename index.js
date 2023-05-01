f660a304db568496b84ec8b1f5705e6e459a9c96
muhiminulalvi <eee1916039brur@gmail.com>
2023-05-01 18:26:36 +0600

    added

--- index.js
+++ index.js
@@ -22,6 +22,24 @@ app.get("/news", (req,res) => {
     res.send(news)
 })
 
+app.get("/news/:id", (req,res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedNews = news.find(n => n._id == id)
+    res.send(selectedNews)
+})
+
+app.get("/categories/:id", (req,res)=> {
+    const id = req.params.id;
+    if(id == 0){
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id == id);
+        res.send(categoryNews)
+    }
+})
+
 app.listen(5000, () => {
   console.log("server is running on port 5000");
 });
