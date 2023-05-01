d91df9012fcaf728b3d5e055a0d0979d96fcbc60
muhiminulalvi <eee1916039brur@gmail.com>
2023-05-01 17:28:53 +0600

    added

--- index.js
+++ index.js
@@ -8,12 +8,18 @@ app.use(cors())
 
 const categories = require('./data/categories.json')
 
+const news = require('./data/news.json')
+
 app.get("/", (req, res) => {
   res.json({ message: "Dragon server is running" });
 });
 
 app.get("/categories", (req,res) => {
     res.send(categories)
+})
+
+app.get("/news", (req,res) => {
+    res.send(news)
 })
 
 app.listen(5000, () => {
